0b9667fb4832c6b56cdb302ab02f4592bbf7c9a1
HyunupKang <upup1227@naver.com>
2021-06-16 21:48:50 +0900

    문자열을 공백 기준으로 짜르고 단어 들어가있는 개수 출력

---
+++ 0616_6_1152_단어의 개수.py
@@ -0,0 +1,6 @@
+print(len(input().split()))
+
+# input으로 The curious case of benjamin button을 입력하고
+# 문자열을 공백을 기준으로 짤라주면
+# [The, curious, case, of, benjamin, button]
+# len()함수로 이 list의 길이(들어가 있는 갯수) 출력
